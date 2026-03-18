6f40e149a22c8dbfb05760bc163aaf13eab67f0d
Jonathan <jonathanyanagita@gmail.com>
2026-03-18 10:14:50 -0300

    refator: remove toEntity mapping from user

--- backend/src/main/java/flashcard/app/flashcard/Mapper/UserMapper.java
+++ backend/src/main/java/flashcard/app/flashcard/Mapper/UserMapper.java
@@ -1,17 +1,11 @@
 package flashcard.app.flashcard.Mapper;
 
-import flashcard.app.flashcard.Dto.UserDtos.UserCreateDto;
 import flashcard.app.flashcard.Dto.UserDtos.UserGetDto;
 import flashcard.app.flashcard.Entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    @Mapping(source = "email", target = "email")
-    @Mapping(source = "password", target = "password")
-    User toEntity(UserCreateDto userCreateDto);
 
     UserGetDto toUserGetDto(User toUser);
 
