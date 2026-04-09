7769622e4c3062b7450f5a0343a04798187c109b
Jonathan <jonathanyanagita@gmail.com>
2026-04-09 15:54:35 -0300

    test: update application-test.properties with mailconfig

--- backend/src/test/java/flashcard/app/flashcard/FlashcardApplicationTests.java
+++ backend/src/test/java/flashcard/app/flashcard/FlashcardApplicationTests.java
@@ -2,8 +2,10 @@ package flashcard.app.flashcard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FlashcardApplicationTests {
 
 	@Test
